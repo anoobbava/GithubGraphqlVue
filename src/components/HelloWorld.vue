54dd1081b8b16885e03a51e5fb95c58f7fed274f
Anoob Bava <anoob.bava@gmail.com>
2019-06-14 10:47:02 +0530

    added base query to test the graphql

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,19 @@
 <template>
-  <h3>welcome</h3>
+  <h3>{{jobs}}</h3>
 </template>
 
 <script>
+import { Sample } from '../queries/sample'
+// import gql from 'graphql-tag'
 export default {
-
+  apollo: {
+    jobs: Sample
+  },
+  data () {
+    return {
+      jobs: []
+    }
+  }
 }
 </script>
 
--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import { createApolloClient, restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // Install the vue plugin
 Vue.use(VueApollo)
@@ -9,15 +10,12 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://localhost:4000/graphql'
+const httpEndpoint = 'https://api.graphql.jobs/'
 
 // Config
 const defaultOptions = {
   // You can use `https` for secure connection (recommended in production)
   httpEndpoint,
-  // You can use `wss` for secure connection (recommended in production)
-  // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://localhost:4000/graphql',
   // LocalStorage token
   tokenName: AUTH_TOKEN,
   // Enable Automatic Query persisting with Apollo Engine
@@ -26,7 +24,7 @@ const defaultOptions = {
   // You need to pass a `wsEndpoint` for this to work
   websocketsOnly: false,
   // Is being rendered on the server?
-  ssr: false
+  ssr: false,
 
   // Override default apollo link
   // note: don't override httpLink here, specify httpLink options in the
@@ -35,6 +33,9 @@ const defaultOptions = {
 
   // Override default cache
   // cache: myCache
+  cache: new InMemoryCache({
+    freezeResults: false
+  })
 
   // Override the way the Authorization header is set
   // getAuth: (tokenName) => ...
