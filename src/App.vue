0d8328be4c0e434ce5230eeb189759221c227c3b
Anoob Bava <anoob.bava@gmail.com>
2019-06-20 03:38:41 +0530

    added toolbar for the appl

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-content>
+      <toolBar/>
       <router-view/>
     </v-content>
   </v-app>
 </template>
 <script>
+import toolBar from './components/toolBar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    toolBar
+  }
 }
 </script>
---
+++ src/components/toolBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-toolbar dark color="primary">
+    <v-toolbar-title class="white--text">
+      <v-btn icon>
+        <v-icon medium>home</v-icon>
+      </v-btn>GitHub Graphql
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <v-text-field
+      placeholder="please enter the Github Repo name"
+      ></v-text-field>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
