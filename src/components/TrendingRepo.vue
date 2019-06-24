b93ab607c95d8e59d1c4013d71ea8197195df223
Anoob Bava <anoob.bava@gmail.com>
2019-06-24 22:43:00 +0530

    design for the trending repo component completed

--- src/components/TrendingRepo.vue
+++ src/components/TrendingRepo.vue
@@ -1,21 +1,48 @@
 <template>
-  <h3>{{repository.issues}}</h3>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex v-for="i in 24" :key="`3${i}`" xs3>
+        <v-card>
+          <v-card-title class="cardColor white--text justify-center">
+            <h2>Repo Name</h2>
+          </v-card-title>
+          <v-avatar
+          :size="100"
+          color="grey lighten-4"
+        >
+          <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+        </v-avatar>
+          <v-card-actions class="justify-center">
+          <!-- <v-btn flat color="">100 <v-icon>visibility</v-icon></v-btn> -->
+          <h3>100 <v-icon>visibility</v-icon></h3>
+          <v-btn flat color="primary">View</v-btn>
+          <!-- <v-btn flat color="">100 <v-icon>favorite</v-icon></v-btn> -->
+          <h3>100 <v-icon>favorite</v-icon></h3>
+        </v-card-actions>
+        </v-card>
+        <v-spacer></v-spacer>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
+
 <script>
-import { trendingRepoDetails } from '../queries/trendingRepoDetailsQuery'
+import { trendingRepoDetails } from "../queries/trendingRepoDetailsQuery";
 export default {
   apollo: {
     repository: trendingRepoDetails
   },
-  data () {
+  data() {
     return {
       repository: []
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
-
+.cardHeight {
+  height: 120px;
+}
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,12 @@ import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
-  iconfont: 'md'
+  iconfont: 'md',
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c',
+    cardColor: '#00a7d0'
+  }
 })
