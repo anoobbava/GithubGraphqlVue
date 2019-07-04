0c51b15bc62fad006b53158aaa5a4d55306b7c81
Anoob Bava <anoob.bava@gmail.com>
2019-07-04 20:00:59 +0530

    added Ruby as when page loaded

--- src/components/TrendingRepo.vue
+++ src/components/TrendingRepo.vue
@@ -10,10 +10,10 @@
 
     </div>
     <v-layout row wrap v-else>
-      <v-flex v-for="i in repository.issues.edges" :key="i.id" xs3>
+      <v-flex v-for="i in search.edges" :key="i.id" xs3>
         <v-card>
           <v-card-title class="cardColor white--text justify-center">
-            <h2>{{i.node.title}}</h2>
+            <h2>{{i.node.name}}</h2>
           </v-card-title>
           <v-avatar
           :size="100"
@@ -22,11 +22,9 @@
           <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
         </v-avatar>
           <v-card-actions class="justify-center">
-          <!-- <v-btn flat color="">100 <v-icon>visibility</v-icon></v-btn> -->
-          <h3>100 <v-icon>visibility</v-icon></h3>
+          <h3>{{i.node.stargazers.totalCount}} <v-icon>visibility</v-icon></h3>
           <v-btn flat color="primary">View</v-btn>
-          <!-- <v-btn flat color="">100 <v-icon>favorite</v-icon></v-btn> -->
-          <h3>100 <v-icon>favorite</v-icon></h3>
+          <h3>{{i.node.forks.totalCount}} <v-icon>favorite</v-icon></h3>
         </v-card-actions>
         </v-card>
         <v-spacer></v-spacer>
@@ -41,18 +39,16 @@ export default {
 
   data () {
     return {
-      repository: [],
-      owner: '',
-      name: ''
+      search: [],
+      queryString: ''
     }
   },
   apollo: {
-    repository: {
+    search: {
       query: TRENDING_REPO_DETAILS_QUERY,
       variables () {
         return {
-          owner: 'octocat',
-          name: 'Hello-World'
+          queryString: "language:ruby stars:>10000"
         }
       }
     }
--- src/queries/trendingRepoDetailsQuery.js
+++ src/queries/trendingRepoDetailsQuery.js
@@ -1,12 +1,17 @@
 import gql from 'graphql-tag'
 export const TRENDING_REPO_DETAILS_QUERY = gql`
-query TrendingRepoDetailQuery($owner: String!, $name: String!) {
-  repository(owner: $owner, name: $name) {
-    issues(last:20, states:CLOSED) {
-      edges {
-        node {
-          title
-          url
+query SearchMostTop10Star($queryString: String!) {
+  search(query: $queryString, type: REPOSITORY, first: 40) {
+    edges {
+      node {
+        ... on Repository {
+          name
+          stargazers {
+            totalCount
+          }
+          forks {
+            totalCount
+          }
         }
       }
     }
